05396fbe1b03d117ae69278c36d8a3e998c22e6d
rcabrera2289 <rebecca.cabrera222@gmail.com>
2023-03-30 09:27:30 -0400

    Rebecca update psvm

--- src/main/java/AmericanAirlines/AmericanHomepage.java
+++ src/main/java/AmericanAirlines/AmericanHomepage.java
@@ -3,10 +3,16 @@ package AmericanAirlines;
 import base.CommonAPI;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 
 public class AmericanHomepage extends CommonAPI {
+    public static void main(String[] args) {
+
+    }
 
     public AmericanHomepage(WebDriver driver){
         PageFactory.initElements(driver, this);
     }
 }
+
