ad2f8671d7a13011a60f902c10d522b8e72604a5
Mashrur Islam <mashrurislamsw6@gmail.com>
2023-04-04 23:55:16 +0600

    added test case contact page submission

--- src/test/java/Statefarm/TestStateFarmHomepage.java
+++ src/test/java/Statefarm/TestStateFarmHomepage.java
@@ -1,7 +1,9 @@
 package Statefarm;
 
+import StateFarm.ContactUsPage;
 import StateFarm.StateFarmHomepage;
 import base.CommonAPI;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -17,5 +19,21 @@ public class TestStateFarmHomepage extends CommonAPI {
     public void testBrokenLinks() {
         StateFarmHomepage homepage = new StateFarmHomepage(getDriver());
         homepage.checkForBrokenLinks();
+    }
+    @Test
+    public void testContactUs() {
+        StateFarmHomepage stateFarmHomePage = new StateFarmHomepage(getDriver());
+
+        // Click the "Contact Us" link on the homepage
+        ContactUsPage contactUsPage = stateFarmHomePage.clickContactUsLink();
+
+        // Fill out the "Contact Us" form
+        contactUsPage.fillOutForm("Billing", "John", "Doe", "johndoe@example.com", "123-456-7890", "I have a question about my billing.");
+
+        // Submit the form
+        contactUsPage.submitForm();
+
+        // Verify that the message was sent successfully
+        Assert.assertTrue(contactUsPage.isSentSuccessMessageDisplayed());
     }
 }
